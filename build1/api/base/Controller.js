3977d7d5c917e54217f651b86246ec9812ca3709
Trevor Overman <troverman@gmail.com>
2015-07-22 05:05:21 -0400

    upd8

---
+++ build1/api/base/Controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
+
+/**
+ * BaseController.js
+ *
+ * Base controller for all sails.js controllers. This just contains some common code
+ * that every controller uses
+ */
+module.exports = {
+ 
+};
---
+++ build1/api/base/Model.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * api/base/model.js
+ *
+ * Base model for all sails.js models. This just contains some common code that every "nearly" every model uses.
+ */
+module.exports = {
+ 
+};
